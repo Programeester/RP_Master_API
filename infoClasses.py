1654f611f152286349df3d44a0443fd002ccd687
programmeester <87316911+Programeester@users.noreply.github.com>
2022-02-15 13:28:24 +0100

    Update infoClasses.py

--- infoClasses.py
+++ infoClasses.py
@@ -1,4 +1,14 @@
 from pydantic import BaseModel as bm
 
+class UserInfo(bm):
+  user_id : str
+  name : str
+  xp : int
+  level : int
+
+
 class TagInfo(bm):
-  
+  server : str
+  creator : str
+  name : str
+  content : str
