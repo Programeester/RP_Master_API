150eea97acae292993eb43fe97b0f419e4ee27cb
programmeester <87316911+Programeester@users.noreply.github.com>
2022-02-15 12:41:26 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+from typing import Optional as opt
+from database import connectToDatabase
+from models import *
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def start_app():
+  await connectToDatabase()
+
+@app.get("/")
+async def home():
+  return {"online" : True}
+
+register_tortoise(
+  app,
+  db_url="sqlite://db.sqlite3",
+  modules={"models": ["models"]},
+  generate_schemas=True,
+  add_exception_handlers=True,
+)
