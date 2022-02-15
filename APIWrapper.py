326cc14fa3e20281d31dfe224810256303908572
programmeester <87316911+Programeester@users.noreply.github.com>
2022-02-15 16:43:38 +0100

    Update APIWrapper.py

--- APIWrapper.py
+++ APIWrapper.py
@@ -5,3 +5,9 @@ URL = "https://rpmasterapi-production.up.railway.app"
 def create_user(user):
   response = request.post(url, data=dict(user))
   return response
+
+def create_tag(tag):
+  response = request.post(url, data=dict(tag))
+  return response
+
+def
